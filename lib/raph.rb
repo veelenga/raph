3d4f9f99dbf6d033afd0ff3c3ba88c1370382667
Vitalii Elenhaupt <velenhaupt@gmail.com>
2015-01-31 17:14:51 +0200

    Basic rspec

--- lib/raph.rb
+++ lib/raph.rb
@@ -1,6 +1,2 @@
 require 'raph/version'
 require 'raph/arg_list'
-
-module Raph
-  # Your code goes here...
-end
---
+++ spec/raph_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Raph do
+  it "should pass" do
+    # pass
+  end
+end
